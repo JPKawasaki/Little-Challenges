e018d4a3181ad254a26563eab29616a6206111ed
JPKawasaki <joshuapascallambertz@gmail.com>
2025-05-16 14:38:39 +0200

    LittleChallenges

---
+++ InteractiveWorldMap/Code/JS/InteractiveWorldMap.js
@@ -0,0 +1,6 @@
+const map = L.map('map').setView([20, 0], 2);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; OpenStreetMap contributors | © Carto',
+  maxZoom: 19
+}).addTo(map);
