b506c8ebba5ded65f7e2328ec7429cd68c4bcae8
JPKawasaki <joshuapascallambertz@gmail.com>
2025-05-22 20:08:07 +0200

    LittleChallenges

---
+++ Calculator/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded'), () => {
+    const result = document.getElementById('result');
+    const value = document.getElementById('formula');
+    const operator = document.getElementById('operator');
+
+    const calculation = function(value) {
+        
+    }
+}
