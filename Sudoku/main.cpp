68d3c56470c4890a314cfd90e13c2fc538a1b11c
JPKawasaki <joshuapascallambertz@gmail.com>
2025-06-25 13:28:10 +0200

    Changed grid

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -1,5 +1,29 @@
 //This will be my soduko App with C++
+//Need to use Backtracking for checking the 9x9 Arrays
+
 #include <iostream>
+#include <string.h>
 using namespace std;
 
-int squareOne = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+int main() {
+    int grid[9][9] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+    
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << grid[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
