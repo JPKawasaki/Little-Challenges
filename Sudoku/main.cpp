951978f780cf7c8a46683f1b9103bc956f58c78f
JPKawasaki <joshuapascallambertz@gmail.com>
2025-06-26 13:50:15 +0200

    Can generate a functional Sudoku now

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -1,5 +1,5 @@
 //This will be my soduko App with C++
-//Need to use Backtracking for checking the 9x9 Arrays
+//Need to use Backtracking for checking the 9x9 Grids
 /*First, you must have a sudoku solver. Apply the solver on an empty sudoku. That is to find a solution for a sudoku with no clues.
 Filling in numbers from top left to right bottom in order. Otherwise, there is a time issue. I don't know why.
 Anyway, it works very fast wait no time to have a completed sudoku puzzle. When you apply backtracking,
@@ -10,11 +10,6 @@ try removing numbers from the above puzzle. Everytime, you remove a number, you 
 If it has more than one solution. The number should put back and try next position in the random list.
 This process continues until the end of list or you already has removed 64 numbers from the puzzle successfully.
 The number is 64 because somebody has proved that there is no sudoku with less than 17 clues with unique solution.
-This process is varies from 15 seconds to 2 minutes. Usually 30 seconds to get a sudoku puzzle.
-
-Third, if you don't want to wait 30 seconds to 2 minutes for each sudoku puzzle, you may apply some mutations to the above sudoku.
-This includes switching rows and columns, rotating. You may also remap the numbers. For example, 1->2, 2->3...9->1.
-After rotating and remapping, no one will notice this is the original sudoku.
 */
 
 #include <iostream>
@@ -26,12 +21,13 @@ using namespace std;
 
 bool canPlace(int grid[9][9], int row, int col, int num);
 bool solveSudoku(int grid[9][9]);
+bool removeNumbers(int grid[9][9]);
 
 int main() {
     int grid[9][9] = {{0}}; //Fill the grid with 0
 
     //Solve Sodoku
-    if (solveSudoku(grid)) {
+    if (removeNumbers(grid)) {
         cout << "Sudoku solved:\n";
         for (int i = 0; i < 9; i++) { //To print out the grid
             for (int j = 0; j < 9; j++) {
@@ -111,7 +107,61 @@ bool solveSudoku(int grid[9][9]) {
     return false;
 }
 
+int countSolutions(int grid[9][9]) {
+    int row, col;
+    bool emptyFound = false;
+
+
+    //Looking for a empty row or column
+    for (int i = 0; i < 9 && !emptyFound; i++)
+        for (int j = 0; j < 9 && !emptyFound; j++)
+            if (grid[i][j] == 0) {
+                row = i; col = j; emptyFound = true; //emptyFound = true, to stop the searching process
+            }
+
+    if (!emptyFound) return 1;
+
+    int solutions = 1;
+    for (int i = 0; i < 9; i++) {
+        if (canPlace(grid, row, col, i)) {
+            grid[row][col] = i;
+            solutions += countSolutions(grid);
+            grid[row][col] = 0;
+            if (solutions > 1) {
+                return solutions; //Abbruch bei zwei Solutions
+            }
+        }
+    }
+    return solutions;
+}
+
 //Function to remove Numbers and to check if there is only one Solution per deleted number
 bool removeNumbers(int grid[9][9]) {
+    solveSudoku(grid);
 
+    std::vector<int> cells(81);
+    std::iota(cells.begin(), cells.end(), 0); // füllt mit 0..80
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(cells.begin(), cells.end(), g);
+
+    int removedCount = 0;
+
+    for (int pos : cells) {
+        int row = pos / 9;
+        int col = pos % 9;
+        int backup = grid[row][col];
+        grid[row][col] = 0;
+        
+        if (countSolutions(grid) != 1) {
+            grid[row][col] = backup;
+        } else {
+            removedCount++;
+        }
+
+        if (removedCount >= 47) { //64 ist maximum
+            break;
+        }
+    }
+    return true;
 }
